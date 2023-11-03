48580956050ad8ee628e79147e135e2a447ff0a4
Cuong <hungcuongdoanmisis@gmail.com>
2023-11-03 09:58:51 +0700

    1st change

--- script.js
+++ script.js
@@ -6,11 +6,31 @@ const diceImage = document.querySelector('.dice');
 let currentScore = document.querySelectorAll('.current-score');
 let totalScore = document.querySelectorAll('.score');
 let players = document.querySelectorAll('.player');
+let ranNumber, i, scores;
+//////////////////////////////////////
+const endgameSetting = function () {
+  rollDice.classList.add('hidden-button');
+  hold.classList.add('hidden-button');
+  diceImage.classList.add('hidden-button');
+};
 /////////////////////////////////////
-let i = 0;
-let scores = 0;
-let ranNumber = 0;
+const startPlaying = function () {
+  i = 0;
+  scores = 0;
+  ranNumber = 0;
+  for (let j = 0; j < players.length; j++) {
+    currentScore[j].textContent = '0';
+    totalScore[j].textContent = '0';
+    players[j].classList.remove('player--active');
+    players[j].classList.remove('player--winner');
+  }
+  players[0].classList.add('player--active');
+  rollDice.classList.remove('hidden-button');
+  hold.classList.remove('hidden-button');
+  diceImage.classList.remove('hidden-button');
+};
 ////////////////////////////////////
+startPlaying();
 let swapPlayer = function () {
   switch (i) {
     case 0:
@@ -55,16 +75,14 @@ hold.addEventListener('click', function () {
 });
 
 //Click New Game Event
-newGame.addEventListener('click', function () {
-  for (let j; j < players.length; j++) {
-    currentScore[i].textContent = '0';
-    totalScore[i].textContent = '0';
-    players[i].classList.remove('player--active');
-    players[0].classList.add('player--active');
-  }
-});
+newGame.addEventListener('click', startPlaying);
+
+//Condition win
 const winner = function () {
-  if (Number(totalScore[i].textContent) >= 100) {
+  if (Number(totalScore[i].textContent) >= 20) {
     console.log(`Winner is Player${i + 1}`);
+    players[i].classList.add('player--winner');
+    players[i].classList.remove('player--active');
+    endgameSetting();
   }
 };
